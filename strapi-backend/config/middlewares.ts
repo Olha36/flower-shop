fd54274e137f1e90a5471c37c893546a6e9becb8
olha36 <olhakucheruk36@gmail.com>
2026-04-25 10:57:19 +0300

    add cors to strapi

--- strapi-backend/config/middlewares.ts
+++ strapi-backend/config/middlewares.ts
@@ -1,10 +1,26 @@
 import type { Core } from '@strapi/strapi';
 
+const frontendUrl = process.env.FRONTEND_URL;
+
+const corsOrigins = [
+  'http://localhost:3000',
+  'https://localhost:3000',
+  frontendUrl,
+].filter(Boolean);
+
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: corsOrigins,
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
