4b2775a188e8f8af7d0f59476adad79f5353c0c1
olha36 <olhakucheruk36@gmail.com>
2026-05-19 15:21:48 +0300

    save changes

--- frontend/flowers/src/lib/api.ts
+++ frontend/flowers/src/lib/api.ts
@@ -1,17 +1,24 @@
 import type { Flower } from "@/types/flowers";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001";
-const FLOWERS_API_URL = `${API_URL}/flowers`;
+const API_URL = (
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001"
+).replace(/\/+$/, "");
+const FLOWERS_API_URL = `${API_URL}`;
 
 export async function getFlowers(): Promise<Flower[]> {
   const res = await fetch(FLOWERS_API_URL);
+  console.log(res, 'res')
 
   if (!res.ok) {
-    throw new Error("Failed to fetch flowers");
+    throw new Error(
+      `Failed to fetch flowers: ${res.status} ${res.statusText} from ${FLOWERS_API_URL}`
+    );
   }
 
   return res.json();
 }
+
+
 
 export async function getFlowerById(id: string): Promise<Flower | null> {
   const flowers = await getFlowers();
